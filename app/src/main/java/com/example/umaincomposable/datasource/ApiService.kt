5dd8465dca1c6c0fd9be8987136c1000615adda8
Jackson Mafra <jackson.mafra@umain.com>
2023-02-13 09:19:44 +0100

    - Fix Conventions  - Add suspend to function

--- app/src/main/java/com/example/umaincomposable/datasource/ApiService.kt
+++ app/src/main/java/com/example/umaincomposable/datasource/ApiService.kt
@@ -1,11 +1,8 @@
 package com.example.umaincomposable.datasource
 
-import com.example.umaincomposable.models.Restaurant
 import com.example.umaincomposable.models.RestaurantList
-import retrofit2.Call
-import retrofit2.Response
 import retrofit2.http.GET
-
+// Always keep the name of the file as the class name
 interface ApiService {
     @GET("restaurants")
     suspend fun getRestaurants(): RestaurantList
--- app/src/main/java/com/example/umaincomposable/datasource/MainRepository.kt
+++ app/src/main/java/com/example/umaincomposable/datasource/MainRepository.kt
@@ -1,7 +1,8 @@
 package com.example.umaincomposable.datasource
 
-
+// Always keep the name of the file as the class name
 class MainRepository(private val apiHelper: ApiHelper) {
 
+    // This function needs to be suspend as well
     suspend fun getRestaurants() = apiHelper.getRestaurants()
 }
