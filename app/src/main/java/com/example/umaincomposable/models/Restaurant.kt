1ea486d669fb43f2654c6a07abb2d1de2c3e4dec
Jackson Mafra <jackson.mafra@umain.com>
2023-02-13 09:20:05 +0100

    - Fix Conventions

--- app/src/main/java/com/example/umaincomposable/models/Restaurant.kt
+++ app/src/main/java/com/example/umaincomposable/models/Restaurant.kt
@@ -1,22 +1,17 @@
 package com.example.umaincomposable.models
 
-import androidx.compose.ui.graphics.painter.Painter
-import androidx.compose.ui.res.painterResource
-import com.fasterxml.jackson.annotation.JsonProperty
-
 data class Restaurant(
     val id: String,
     val name: String,
     val rating: Float,
     val filterIds: List<String>,
     val imageUrl: String,
-    val deliveryTime: String) {
-
-}
+    val deliveryTime: String
+)
 
 data class RestaurantList(
     val restaurants: List<Restaurant>
 )
-//val restaurants = arrayOf(Restaurant("Beans", arrayOf("Takeout", "Random"), "3.5", "30m"),
+// val restaurants = arrayOf(Restaurant("Beans", arrayOf("Takeout", "Random"), "3.5", "30m"),
 //            Restaurant("Milk", arrayOf("Takeout"), "55", "30m"),
 //            Restaurant("Peach", arrayOf("Takeout", "Random", "Wow!"), "1", "25m"))
