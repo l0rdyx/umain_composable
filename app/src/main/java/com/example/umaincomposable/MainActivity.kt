dd748bf8fd2bcb981b21a51a658a61ae08c28f67
Jackson Mafra <jackson.mafra@umain.com>
2023-02-13 09:24:40 +0100

    - Add uistate  - Added viewModelScope  - handle states

--- app/src/main/java/com/example/umaincomposable/MainActivity.kt
+++ app/src/main/java/com/example/umaincomposable/MainActivity.kt
@@ -7,43 +7,46 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material.Button
 import androidx.compose.material.Text
-import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
-import com.example.umaincomposable.datasource.*
-import com.example.umaincomposable.models.Restaurant
 import com.example.umaincomposable.viewmodels.MainPageModel
-import kotlinx.coroutines.*
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
+    // Take a look on Koin (Dependency Injection)
     var mainPageModel = MainPageModel()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-                Column() {
-                    Row() {
-                        Logo()
-                    }
-                    OptionsLane()
-                    Column() {
-                        RestaurantsList(mainPageModel)
-                    }
+            Column() {
+                Row() {
+                    Logo()
                 }
+                OptionsLane()
+                Column() {
+                    RestaurantsList(mainPageModel)
+                }
+            }
         }
-
     }
 }
-
-
 
 @Composable
 fun Logo() {
     Image(painter = painterResource(id = R.drawable.logo), contentDescription = "logo")
 }
 
-@Composable 
+@Composable
 fun OptionsLane() {
     Row() {
         Button(onClick = { /*TODO*/ }) {
@@ -57,41 +60,61 @@ fun OptionsLane() {
     }
 }
 
-
 @SuppressLint("CoroutineCreationDuringComposition")
 @Composable
 fun RestaurantsList(mainPageModel: MainPageModel) {
-    val coroutineScope = CoroutineScope(Dispatchers.Default)
+    // val coroutineScope = CoroutineScope(Dispatchers.Default)
+    // YOU CAN CHANGE FOR THIS...
+    val scope = rememberCoroutineScope()
 
-    coroutineScope.launch {
-        mainPageModel.getRestaurants()
-        println(mainPageModel.result)
+    val uiState by mainPageModel.uiState.collectAsState()
+
+    Button(onClick = {
+        scope.launch {
+            mainPageModel.getRestaurants()
+        }
+    }) {
+        Text(text = "Load")
     }
-    val res = mainPageModel.result?.restaurants?.toMutableStateList()
-    println("SUPPOSED TO BE HERE")
-    println(res)
-    if (res != null) {
-        for (restaurant in res) {
-            Row(verticalAlignment = Alignment.CenterVertically) {
-                Column() {
-                    Image(painter = painterResource(id = R.drawable.take_out), contentDescription = "")
-                    Row() {
-                        Text(text = restaurant.name)
-                        Image(painter = painterResource(id = R.drawable.star), contentDescription = "")
-                        Text(text = restaurant.rating.toString())
-                    }
-                    Row() {
-                        for (option in restaurant.filterIds) {
-                            Text(text = option)
-                        }
-                    }
-                    Row() {
-                        Image(painter = painterResource(id = R.drawable.clock), contentDescription = "")
-                        Text(text = restaurant.deliveryTime)
+    LazyColumn(
+        modifier = Modifier.fillMaxWidth(),
+        /*
+        rememberLazyListState
+        Changes to the provided initial values will not result in the state being recreated or changed in any way if it has already been created.
+         */
+        state = rememberLazyListState()
+    ) {
+        items(
+            items = uiState.data,
+            key = {
+                it.id
+            }
+        ) { restaurant ->
+            Text(text = restaurant.name)
+        }
+    }
+
+    /*
+    for (restaurant in uiState.data) {
+        Row(verticalAlignment = Alignment.CenterVertically) {
+            Column() {
+                Image(painter = painterResource(id = R.drawable.take_out), contentDescription = "")
+                Row() {
+                    Text(text = restaurant.name)
+                    Image(painter = painterResource(id = R.drawable.star), contentDescription = "")
+                    Text(text = restaurant.rating.toString())
+                }
+                Row() {
+                    for (option in restaurant.filterIds) {
+                        Text(text = option)
                     }
                 }
-
+                Row() {
+                    Image(painter = painterResource(id = R.drawable.clock), contentDescription = "")
+                    Text(text = restaurant.deliveryTime)
+                }
             }
         }
     }
+    */
 }
--- app/src/main/java/com/example/umaincomposable/viewmodels/MainPageModel.kt
+++ app/src/main/java/com/example/umaincomposable/viewmodels/MainPageModel.kt
@@ -1,20 +1,46 @@
 package com.example.umaincomposable.viewmodels
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.umaincomposable.datasource.ApiHelper
 import com.example.umaincomposable.datasource.MainRepository
 import com.example.umaincomposable.datasource.RetrofitBuilder
 import com.example.umaincomposable.models.Restaurant
-import com.example.umaincomposable.models.RestaurantList
-import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 
+data class UiState(
+    val data: List<Restaurant> = emptyList(),
+    val loading: Boolean = true
+) {
+    val isEmpty = !loading && data.isEmpty()
+}
 class MainPageModel : ViewModel() {
-    var result: RestaurantList? = null
-    suspend fun getRestaurants(){
-            val apiHelper: ApiHelper = ApiHelper(RetrofitBuilder.apiService)
+
+    private val _uiState = MutableStateFlow(UiState(loading = true))
+    val uiState: StateFlow<UiState> = _uiState.asStateFlow()
+
+    init {
+        viewModelScope.launch {
+            getRestaurants()
+        }
+    }
+
+    suspend fun getRestaurants() {
+        try {
+            val apiHelper = ApiHelper(RetrofitBuilder.apiService)
             val mainRepository = MainRepository(apiHelper)
-            println("IM HERE")
-            result = mainRepository.getRestaurants()
+            _uiState.update {
+                it.copy(
+                    data = mainRepository.getRestaurants().restaurants
+                )
+            }
+        } finally {
+            Log.d("MainPageModel", "Loading done")
+        }
     }
 }
